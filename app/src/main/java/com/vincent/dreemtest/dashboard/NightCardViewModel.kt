393377ad9a77913d4494596eb41c3ec342ff211d
VincentPlumeDev <vincent@plumelabs.com>
2020-06-14 20:52:08 +0200

    [Dashboard] Added quality, sleep duration and onset duration

--- app/src/main/java/com/vincent/dreemtest/dashboard/NightCardViewModel.kt
+++ app/src/main/java/com/vincent/dreemtest/dashboard/NightCardViewModel.kt
@@ -1,20 +1,29 @@
 package com.vincent.dreemtest.dashboard
 
 import android.content.Context
+import androidx.core.graphics.scaleMatrix
 import com.vincent.dreemtest.R
 import com.vincent.dreemtest.domain.entity.Night
 import org.koin.java.KoinJavaComponent.inject
+import java.time.Duration
+import java.time.LocalTime
+import java.time.Period
+import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 import kotlin.math.roundToInt
 
-data class NightCardViewModel(val id: String, val title: String, val sleepScore: String)
+data class NightCardViewModel(val id: String, val date: ZonedDateTime, val sleepDuration: LocalTime, val sleepOnsetDuration: LocalTime, val quality: Float)
 
 internal fun Night.toCardViewModel(): NightCardViewModel {
-    val context: Context by inject(Context::class.java)
-    val dateFormat = DateTimeFormatter.ofPattern(context.getString(R.string.card_night_date_format))
     return NightCardViewModel(
         id = id,
-        title = recordDateRange.endInclusive.format(dateFormat),
-        sleepScore = context.getString(R.string.card_night_score, (sleepScore * 100f).roundToInt())
+        date = recordDateRange.endInclusive,
+        sleepDuration = recordDateRange.endInclusive.timeSince(recordDateRange.start),
+        sleepOnsetDuration = sleepOnsetDuration.toLocalTime(),
+        quality = sleepScore
     )
 }
+
+private fun ZonedDateTime.timeSince(other: ZonedDateTime): LocalTime = Duration.between(this, other).toLocalTime()
+
+private fun Duration.toLocalTime(): LocalTime = LocalTime.of(toHours().toInt(), toMinutes().toInt(), seconds.toInt())
